2f89689c37e77d6675d6ee956bdda6349edde493
Abraham Zhang <zzhang4979@gmail.com>
2024-01-08 10:38:02 -0500

    Sports Application Website

--- ICS4U/emailValidator.js
+++
@@ -1,8 +0,0 @@
-/* function ValidateEmail(mail) {
-    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
-}
-console.log(ValidateEmail('test@test.com')*/
-
-const  emailValidator = require('email-validator')
-
-console.log(emailValidator.validate('test@test.com'))
